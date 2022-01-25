c8eca8e97ce66247806bc80171897d986b321f7c
Erik Johnson <ejohnson@rhinocorps.com>
2022-01-25 16:44:00 -0700

    new files

---
+++ sicp/hello.scm
@@ -0,0 +1,20 @@
+#!/usr/bin/guile -s
+!#
+; A trivial GUILE script, taken from:
+; https://www.gnu.org/software/guile/manual/html_node/Running-Guile-Scripts.html 
+;
+; With x permissions set, you should be able to execute this script as:
+;   $ hello.scm
+;
+; Or, without, as:
+;   $ guile hello.scm
+;
+; SEE ALSO: 
+;   local README.txt
+;   https://www.gnu.org/software/guile/manual/html_node/index.html
+;   http://community.schemewiki.org/?SICP-Solutions
+
+(display "Hello, world!")
+(newline)
+
+; EOF
