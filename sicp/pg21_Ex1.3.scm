5ba92f861af5624d40f58a9a16aa1ce2fa3ff2ec
Erik Johnson <ejohnson@rhinocorps.com>
2022-01-25 17:04:20 -0700

    starting new exercize

---
+++ sicp/pg21_Ex1.3.scm
@@ -0,0 +1,15 @@
+
+; AUTHOR: Erik Johnson
+;   DATE: 2022Jan25
+;
+; DESCRIPTION
+;    SICP Exercize 1.3, pg 21
+;
+; PROBLEM STATEMENT
+;   Define a procedure that takes three numbers as arguments and returns
+;   the sum of the squares of the two larger numbers.
+
+(define (sq x) (x * x))
+
+(display "TODO")
+(newline)
