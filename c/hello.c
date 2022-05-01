a641b0cc15b84c3dbe6553b8d08c206fd90bc500
Erik Johnson <ejohnson@Eriks-Mac-mini.local>
2022-05-01 13:42:30 -0600

    Some C stuff.

---
+++ c/hello.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+// In this simplest case, this program can be compiled as simply:
+//
+// $ gcc hello.c -o hello
+//
+// And then invoked as:
+//
+// $ ./hello
+//Hello, World!
+//
+
+int main(int argc, char *argv[]) {
+    printf("Hello, World!\n");
+}
