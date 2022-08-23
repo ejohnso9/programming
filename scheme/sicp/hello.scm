b14c63834290e1437095d210637ace0065a4623e
Erik Johnson <ejohnson@rhinocorps.com>
2022-08-22 18:13:09 -0600

    minor commenting

--- scheme/sicp/hello.scm
+++ scheme/sicp/hello.scm
@@ -3,10 +3,12 @@
 ; A trivial GUILE script, taken from:
 ; https://www.gnu.org/software/guile/manual/html_node/Running-Guile-Scripts.html 
 ;
-; With x permissions set, you should be able to execute this script as:
+; With 'x' permissions set, on a machine that has 'guile' installed (such
+; as is the case at pythonanywhere.com) you should be able to execute
+; this script as:
 ;   $ hello.scm
 ;
-; Or, without, as:
+; Or, without 'x' permissions as:
 ;   $ guile hello.scm
 ;
 ; SEE ALSO: 
