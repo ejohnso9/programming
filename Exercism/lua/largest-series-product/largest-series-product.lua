8326443569647855e7de11f65c1edd13c0ca86b1
Erik Johnson <ejohnso9@earthlink.net>
2023-04-07 00:25:05 +0000

    added degnerate cases

--- Exercism/lua/largest-series-product/largest-series-product.lua
+++ Exercism/lua/largest-series-product/largest-series-product.lua
@@ -15,10 +15,15 @@
 --   using 0 for the product.
 
 
+local ASCII_0 = string.byte('0')  -- -> 48
+local ASCII_9 = string.byte('9')  -- -> 57
+
+
 -- NB: does no validation of string: assumed to be only '0' - '9'
 local function spanProduct(s)
     local product = 1  -- multiplicitive identity
-    local ASCII_0 = 48  -- string.byte('0') -> 48
+
+    -- print(string.format("s is: '%s'", s))
 
     -- convert each char in 's' to int value, multiply product by that
     for i = 1, #s do
@@ -30,26 +35,56 @@ local function spanProduct(s)
         end
     end
 
+    -- print(string.format("product is: %s\n", product))
+
     return product
 end
 
 
--- args: config: table w/ keys: 
---  'digits': str - the list of digits to multiply
---  'span': int - how many digits to take at a time
-return function(config)
-    local digits_s = config['digits']
-    local span = config['span']
+local function isAllDigits(s)
+    local ascii = 0
+    for i = 1, #s do
+        ascii = string.byte(s:sub(i, i))
+        if ascii < ASCII_0 or ascii > ASCII_9 then
+            return false end
+    end
+
+    return true
+end
 
 
-    local last_index = #digits - span
-    max = 0
-    for i = 1, last_index do
-        val = spanProduct(digits_s:sub(i, i + span))
+
+-- args_t: table w/ keys: 
+--   'digits': str - the list of digits to multiply
+--   'span': int - how many digits to take at a time
+local function largest_product(args_t)
+    local digits_s = args_t['digits']
+    local span = args_t['span']  -- int
+    local max = 0
+
+    -- degenerate case handling
+    if span == 0 and #digits_s > 0 then
+        return 1
+    elseif span > #digits_s then
+        error("span larger than digits")
+    elseif #digits_s == 0 and span > 0 then
+        error("no digits and non-zero span")
+    elseif span < 0 then
+        error("negative span")
+    elseif not isAllDigits(digits_s) then
+        error("found chars outside '0' - '9'")
+    end
+
+    -- inputs validated, compute the product
+    for i = 1, #digits_s - span + 1 do
+        val = spanProduct(digits_s:sub(i, i + span - 1))
         if val > max then
             max = val end
     end
 
     return max
 end
+
+
+return largest_product  -- function
 
