c66d589446042fdae40a990a871490c34b8f1d99
Erik Johnson <ejohnso9@earthlink.net>
2025-07-28 22:15:16 +0000

    scheme update

--- scheme/sicp/pg21_Ex1.5.scm
+++ scheme/sicp/pg21_Ex1.5.scm
@@ -4,19 +4,23 @@
 ; SICP(2E) Problem 1.5, page 21
 ; AUTHOR: Erik Johnson
 ; DATE: 2022Jan25
+;       2025Mar13 Looking at SICP again, and so I guess this exercize
+;       drives hom the point that the interpreter is in fact
+;       "applicative" order and not "normal" order as described on pages
+;       15-16.
 
 (define (p) (p))
 
 (define (test x y)
    (if (= x 0) 0 y) )
 
+(newline)
+(display "just before calling: (test 0 (p))  ; (This will, in fact, hang. Hit CTRL-C to exit.")
 
 ; This *WILL* in fact hang. I suppose it will eventually crash, but
-; after leaving it run for like 30 seconds, it had not yet crashed.
-;
-; (test (p) 0)  ; uncomment this line if you really have to see it.
+; after leaving it run for a couple minutes, it had not yet crashed.
+(test 0 (p))  ; uncomment this line if you really have to see it.
 
-(newline)
-(display "call: (test (p) 0)")
-(display "...will, in fact, hang. Edit source to verify.")
+(display "just after (test 0 (p))")
+
 (newline)
