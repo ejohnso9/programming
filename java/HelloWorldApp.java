ac5e46f0dfc77fcb6499f0023de6d5003601e1e1
Erik Johnson <ejohnso9@earthlink.net>
2025-07-28 22:12:38 +0000

    new java dir

---
+++ java/HelloWorldApp.java
@@ -0,0 +1,13 @@
+
+// https://docs.oracle.com/javase/tutorial/displayCode.html?code=https://docs.oracle.com/javase/tutorial/getStarted/application/examples/HelloWorldApp.java
+
+/**
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+class HelloWorldApp {
+    public static void main(String[] args) {
+        // Display the string.
+        System.out.println("Hello, World!");
+    }
+}
