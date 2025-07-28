dc6e58e40a4cfcb6006c7f463a15684ef8bb2536
Erik Johnson <ejohnso9@earthlink.net>
2025-07-28 16:42:32 -0600

    old Exercism stuff in F#

---
+++ Exercism/fsharp/annalyns-infiltration/AnnalynsInfiltration.fs
@@ -0,0 +1,28 @@
+module AnnalynsInfiltration
+
+(*
+to submit, from within this dir, simply:
+
+    $ exercism submit
+*)
+
+
+let canFastAttack (knightIsAwake: bool): bool =
+    not knightIsAwake
+
+let canSpy (knightIsAwake: bool) (archerIsAwake: bool) (prisonerIsAwake: bool): bool =
+    knightIsAwake || archerIsAwake || prisonerIsAwake
+
+let canSignalPrisoner (archerIsAwake: bool) (prisonerIsAwake: bool): bool =
+    prisonerIsAwake && not archerIsAwake
+
+let canFreePrisoner (knightIsAwake: bool) (archerIsAwake: bool) (prisonerIsAwake: bool) (petDogIsPresent: bool): bool =
+    (petDogIsPresent && not archerIsAwake)
+    || (
+        not petDogIsPresent
+        && prisonerIsAwake
+        && not knightIsAwake
+        && not archerIsAwake)
+
+// EOF
+
--- Exercism/fsharp/lucians-luscious-lasagna/LuciansLusciousLasagna.fs
+++ Exercism/fsharp/lucians-luscious-lasagna/LuciansLusciousLasagna.fs
@@ -1,5 +1,35 @@
 module LuciansLusciousLasagna
 
+(*
+AUTHOR
+    Erik Johnson
+
+DESCRIPTION
+    Solution to Exercism problem: 
+    https://exercism.org/tracks/fsharp/exercises/lucians-luscious-lasagna
+
+HISTORY
+--------
+2023Jan10 ej created
+2024Aug27 ej updating comments, revisiting this whole FS thing
+             retest via:  $ dotnet test  // STILL PASSING
+
+(base) Eriks-Mac-mini:lucians-luscious-lasagna ejohnson$ dotnet test
+  Determining projects to restore...
+  All projects are up-to-date for restore.
+  LuciansLusciousLasagna -> /Users/ejohnson/src/git/programming/Exercism/fsharp/lucians-luscious-lasagna/bin/Debug/net7.0/LuciansLusciousLasagna.dll
+Test run for /Users/ejohnson/src/git/programming/Exercism/fsharp/lucians-luscious-lasagna/bin/Debug/net7.0/LuciansLusciousLasagna.dll (.NETCoreApp,Version=v7.0)
+Microsoft (R) Test Execution Command Line Tool Version 17.4.0 (x64)
+Copyright (c) Microsoft Corporation.  All rights reserved.
+
+Starting test execution, please wait...
+A total of 1 test files matched the specified pattern.
+
+Passed!  - Failed:     0, Passed:     6, Skipped:     0, Total:     6, Duration: 7 ms - LuciansLusciousLasagna.dll (net7.0)
+
+*)
+
+
 // define the 'expectedMinutesInOven' binding
 let expectedMinutesInOven = 40
 
@@ -14,3 +44,4 @@ let preparationTimeInMinutes nLayers =
 // define the 'elapsedTimeInMinutes' function
 let elapsedTimeInMinutes nLayers bakedMin =
     preparationTimeInMinutes(nLayers) + bakedMin
+
