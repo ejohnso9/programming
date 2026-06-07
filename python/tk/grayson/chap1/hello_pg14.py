f3db407ce96952560b350169473c3b00c5783013
Erik Johnson <ejohnso9@earthlink.net>
2026-06-07 17:21:56 -0600

    new file

---
+++ python/tk/grayson/chap1/hello_pg14.py
@@ -0,0 +1,18 @@
+#!/usr/bin/.env python
+
+"""
+DESCRIPTION
+    "Hello World" GUI app from John Grayson's
+    "Python and Tkinter Programming", PG14, PG14, PG14, PG14
+
+    NB: code slightly modified to deal with modern Python 3
+        (e.g., from tkinter, not: from Tkinter)
+
+HISTORY
+    20126Jun07  typed in, tested: works! 
+"""
+
+from tkinter import Label, mainloop
+
+Label(text="This has to be the\nsimplest bit of code").pack()
+mainloop()
