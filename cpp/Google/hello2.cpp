807b02a9883fea60632b8f598b2f82db1d956401
Erik Johnson <ejohnso9@earthlink.net>
2026-07-23 22:10:25 +0000

    newer changes?

--- cpp/Google/hello2.cpp
+++ cpp/Google/hello2.cpp
@@ -1,20 +1,45 @@
-// Orig code from Google Tutorial
-//     https://developers.google.com/edu/c++/getting-started
+// C++ (use g++)
+// file: hello2.cpp
 //
-// 2025Sep24
-// Author: Erik Johnson (no relation to Maggie Johnson)
+/*
+DESCRIPTION
+
+Google C++ Tutorial
+    https://developers.google.com/edu/c++/getting-started
+
+2025Sep24, 2026Mar11
+Author: Erik Johnson (no relation to Maggie Johnson)
+
+This can be compiled w:
+$ g++ hello2.cpp -o hello2
 
 
-// hello.cpp: Maggie Johnson
+2026Mar11
+Google appears to have updated their published code?
+
+// hello2.cpp: Maggie Johnson
 // Description: a program that prints the immortal saying "hello world"
-//
-// #include <iostream>
-// using namespace std;
-// 
-// int main() {
-//   cout << "Hello World!" << endl;
-//   return 0;
-// }
+// many times
+#include <iostream>
+// we need the following include for setw() in some c++ implementations
+#include <iomanip>
+using namespace std;
+
+int main() {
+  // the first for-loop will handle the rows
+  for (int i = 0; i < 6; i++) {
+    // the second for loop will handle the columns
+    for (int j = 0;  j < 4 ; j++)
+      // setw(int) sets the column width
+      cout << setw(17) << "Hello World!";
+    // this  next line is a part of the first for loop
+    // and causes the new line
+    cout << endl;
+  }
+  return 0;
+}
+
+*/
 
 
 #include <iostream>
@@ -33,33 +58,25 @@ int main() {
     string hello = "Hello World!";
 
     // default aligned (right)
-    cout << "default alignment..." << endl;
+    // cout << "default alignment..." << endl;
+    // ej: Does this really do anything?
+    // A: 2026Mar11 Yes, w/ a formatted field (not with default output
+    // of just 'hello').
+    cout << std::setiosflags(ios::left);
+
+    // for 6 lines...
     for (int line_i = 0; line_i < 6; line_i++) {
+        // print "Hello World!" 4 times on a line
         for (int col_i = 0; col_i < 4; col_i++) {
+            // 2026Mar11 This certainly does, though: each HW formatted
+            // to 17 chars wide
             cout << setw(17) << hello;
         }
         cout << endl;
     }
     cout << endl;
 
-    // left aligned
-    // For the record, the given solution uses this at the top of main()
-    //     set up cout to right-align
-    //     cout <<  std::setiosflags(ios::left);
-    // A) The comment is confused.
-    // B) Appears to be global until set otherwise, whereas code below
-    //    would presumably only apply left alignment to the one output.
-    //    That seems a lot better?
-    cout << "using left alignment..." << endl;
-    for (int line_i = 0; line_i < 6; line_i++) {
-        for (int col_i = 0; col_i < 4; col_i++) {
-            cout << setw(17) << left << hello;
-        }
-        cout << endl;
-    }
-    cout << endl;
-
-    return 0;
+    return 0; // int (as required)
 }
 
 // EOF
