a3249e3edc5c7f380422266af2dbd1998898a1e0
Erik Johnson <ejohnso9@earthlink.net>
2026-07-27 17:24:22 +0000

    a minimal C++ program w/ no input or output

---
+++ cpp/tour/min.cpp
@@ -0,0 +1,14 @@
+/**
+ * file: min.cpp
+ *
+ * The 'minimal' C++ program. It is a program that takes no input,
+ * produces no output, but actually runs w/o error.
+ *
+ * This can be compiled (on PythonAnywhere) via:
+ *    $ g++ min.cpp  # should create a.out
+ *
+ * then:
+ *    $ ./a.out  # should return silently
+ */
+int main() { }
+
